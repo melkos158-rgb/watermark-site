448a3b76ddc02f007c20a3a4be6b51962bbaa15f
melkos158-rgb <melkos158@gmail.com>
2025-09-15 01:41:23 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE,
-    name TEXT,
-    avatar_url TEXT,
-    about TEXT,
-    pxp INTEGER NOT NULL DEFAULT 0,
-    code TEXT
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT UNIQUE NOT NULL,
+  name TEXT,
+  avatar TEXT,
+  bio TEXT,
+  pxp INTEGER NOT NULL DEFAULT 0,
+  name_changes INTEGER NOT NULL DEFAULT 0,
+  password TEXT NOT NULL,
+  login_code TEXT
 );
+CREATE UNIQUE INDEX IF NOT EXISTS ux_users_login_code ON users(login_code);
